df3f7c5aa499cbe40f3c4320d96f6c982fd722e8
Riya2919 <96763019+Riya2919@users.noreply.github.com>
2022-12-21 12:01:52 +0530

    ui design

---
+++ app/src/main/java/com/example/zsfinal/dashboard.java
@@ -0,0 +1,49 @@
+package com.example.zsfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class dashboard extends AppCompatActivity {
+    Button medcam, appt, docreview, predict;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        medcam = findViewById(R.id.medcam);
+        appt = findViewById(R.id.appointment);
+        docreview = findViewById(R.id.docreview);
+        predict = findViewById(R.id.predict);
+        medcam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), predictmed.class);
+                startActivity(i);
+            }
+        });
+        appt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), appointment.class);
+                startActivity(i);
+            }
+        });
+        docreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), predictdisease.class);
+                startActivity(i);
+            }
+        });
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), symptoms.class);
+                startActivity(i);
+            }
+        });
+    }
+}
